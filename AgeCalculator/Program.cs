3c1f31f8eda56181f52b885a47d61d549b39fe14
Manuel Riezebosch <mriezebosch@gmail.com>
2014-05-22 10:14:12 +0200

    console application implementatie zonder testen

---
+++ AgeCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Voer de geboortedatum in: ");
+            var birth = DateTime.Parse(Console.ReadLine(), CultureInfo.CurrentCulture);
+
+            var age = CalcAge(birth);
+            Console.WriteLine("De leeftijd is: {0}", age);
+        }
+
+        private static int CalcAge(DateTime birth)
+        {
+            var now = DateTime.Now;
+            var result = now.Year - birth.Year;
+
+            if (now.Month < birth.Month)
+            {
+                result--;
+            }
+
+            if (now.Month == birth.Month && now.Day < birth.Day)
+            {
+                result--;
+            }
+
+            return result;
+        }
+    }
+}
