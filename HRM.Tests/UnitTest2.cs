5417a8110c46f3708cf65700dcc9c6c3d25df54f
Manuel Riezebosch <mriezebosch@gmail.com>
2014-05-23 14:02:10 +0200

    moq quiz met setup en references

--- HRM.Tests/UnitTest2.cs
+++ HRM.Tests/UnitTest2.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.IO;
 
 namespace HRM.Tests
 {
@@ -28,5 +29,35 @@ namespace HRM.Tests
             //mock.Verify();
             mock.Verify(m => m.Zoek(402), Times.Exactly(2900));
         }
+
+        public interface IInterface
+        {
+            void Dummy(FileInfo f);
+        }
+
+        [TestMethod]
+        public void MoqSetupQuiz_Fail()
+        {
+            var mock = new Mock<IInterface>(MockBehavior.Strict);
+            mock.Setup(m => m.Dummy(new FileInfo("C:\\temp\\temp.log")));
+
+            mock.Object.Dummy(new FileInfo("C:\\temp\\temp.log"));
+        }
+
+
+        [TestMethod]
+        public void MoqSetupQuiz_Succeed()
+        {
+            // Omdat nu de reference gelijk is die door de 
+            // setup en uitvoering gedeeld wordt en daarmee
+            // een match is.
+            var fi = new FileInfo("C:\\temp\\temp.log");
+            var mock = new Mock<IInterface>(MockBehavior.Strict);
+            mock.Setup(m => m.Dummy(fi));
+
+            mock.Object.Dummy(fi);
+        }
+
+
     }
 }
